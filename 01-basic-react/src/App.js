4376c3a6572800621c333bd784d8d4093e2a99c9
theriakhurana <theriakhurana@gmail.com>
2025-09-07 15:52:58 +0530

    starting with react app

---
+++ 01-basic-react/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>chai aur react | Ria Khurana</h1>
+  );
+}
+
+export default App;
---
+++ 01-vite-project/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>chai aur react with vite | Ria Khurana</h1>
+  )
+}
+
+export default App
